ad86dd3e0c05a2cf6562f0fa6d65e0d57207b926
rayan <2104303I@student.tp.edu.sg>
2023-12-11 18:09:30 +0800

    db

---
+++ src/db.py
@@ -0,0 +1,6 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+cred = credentials.Certificate(r'C:\Users\S531FL-BQ559T\OneDrive\Documents\MP\Project\MP\src\finsaver3-firebase-adminsdk-udjjx-b479ad6c2d.json')
+firebase_admin.initialize_app(cred)
+db = firestore.client()
--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCZBItJceEEfX_o1zpS1tEkJA41Z1Xn6j8",
-  authDomain: "finsaver2.firebaseapp.com",
-  projectId: "finsaver2",
-  storageBucket: "finsaver2.appspot.com",
-  messagingSenderId: "216403614393",
-  appId: "1:216403614393:web:5c18ca184902b5afe3d968",
-  measurementId: "G-VTJWG3ZSMJ"
+  apiKey: "AIzaSyCwckzqjSuDBdPvFGmwxW_t86FMcaiFYOs",
+  authDomain: "finsaver3.firebaseapp.com",
+  projectId: "finsaver3",
+  storageBucket: "finsaver3.appspot.com",
+  messagingSenderId: "546832209179",
+  appId: "1:546832209179:web:8be50246cdaf1bdc53efbe",
+  measurementId: "G-365RF89GXF"
 };
 
 // Initialize Firebase
