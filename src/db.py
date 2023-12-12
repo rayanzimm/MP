8390ca283bf8915631d6ae32bd8da08abd3b4a6f
Tendou45 <quekasriel02.gmail.com>
2023-12-12 13:52:27 +0800

    js

--- src/db.py
+++ src/db.py
@@ -1,6 +1,15 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
-cred = credentials.Certificate(r'C:\Users\S531FL-BQ559T\OneDrive\Documents\MP\Project\MP\src\finsaver3-firebase-adminsdk-udjjx-b479ad6c2d.json')
+cred = credentials.Certificate(r'C:\Poly module\Year 3\MP\Website Code\MP\src\finsaver3-firebase-adminsdk-udjjx-b479ad6c2d.json')
 firebase_admin.initialize_app(cred)
 db = firestore.client()
+data = {
+    'task': 'wash the dishes',
+    'status': 'todo'
+}
+
+doc_ref = db.collection('taskCollection').document()
+doc_ref.set(data)
+
+print('Document ID:', doc_ref.id)
