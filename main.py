4bf3889d3cccc56f8f57ef5983687308df1da583
Tendou45 <quekasriel02.gmail.com>
2023-12-15 14:55:08 +0800

    js

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, session, render_template, request, redirect
 import pyrebase
+import firebase_admin
+from firebase_admin import credentials, firestore
 
 app = Flask(__name__)
 
@@ -17,11 +19,16 @@ config = {
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
+# Use firebase_admin to initialize Firestore
+cred = credentials.Certificate(r'C:\Poly module\Year 3\MP\Website Code\MP\src\finsaver3-firebase-adminsdk-udjjx-b479ad6c2d.json')
+firebase_admin.initialize_app(cred, {'projectId': 'finsaver3'})
+db = firestore.client()
+
 app.secret_key = 'secret'
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    if('user' in session):
+    if 'user' in session:
         return render_template('home.html')
     if request.method == 'POST':
         email = request.form.get('email')
@@ -43,17 +50,33 @@ def register():
     if request.method == 'POST':
         email = request.form.get('email')
         password = request.form.get('password')
+        firstName = request.form.get('firstName')
+        lastName = request.form.get('lastName')
+        dob = request.form.get('dob')
+        address = request.form.get('address')
+        mobile = request.form.get('mobile')
+        
         try:
             user = auth.create_user_with_email_and_password(email, password)
+            
+            # Store additional user information in Firestore
+            user_data = {
+                'email': email,
+                'firstName': firstName,
+                'lastName': lastName,
+                'dob': dob,
+                'address': address,
+                'mobile': mobile
+            }
+            db.collection('users').add(user_data)
+            
             session['user'] = email
             return render_template('login.html')  # Redirect to login page after successful registration
         except Exception as e:
             # Handle registration errors, you might want to display an error message or log the exception
             return render_template('register.html', error_message=str(e))
 
-    return render_template('register.html')  
-
-
+    return render_template('register.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
